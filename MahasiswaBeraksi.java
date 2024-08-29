971f2d91829477ceb8c64547558239c506a04e83
rivaprba <147835771+rivaprba@users.noreply.github.com>
2024-08-29 21:55:30 +0700

    Add files via upload

---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MahasiswaBeraksi {
+    
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        
+        mahasiswa.membaca = "Mahasiswa itu sedang membaca materi perkuliahan";
+        mahasiswa.nyontek = "Mahasiswa terkadang menyontek karena tidak paham materinya";
+        mahasiswa.modifikasi = "Mahasiswa juga kadang memodifikasi tugasnya";
+        
+        System.out.println(mahasiswa.membaca);
+        System.out.println(mahasiswa.nyontek);
+        System.out.println(mahasiswa.modifikasi);
+    }
+}
+
