ceee07b242aea3c0ecb89729449b7137068af1d8
rivaprba <147835771+rivaprba@users.noreply.github.com>
2024-11-26 11:40:31 +0700

    Update MahasiswaBeraksi.java

--- Pertemuan 1/MahasiswaBeraksi.java
+++ Pertemuan 1/MahasiswaBeraksi.java
@@ -1,25 +1,26 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.khalidah.mahasiswaberaksi;
+package tugas1;
 
 /**
  *
- * @author khali
+ * @author ASUS
  */
 public class MahasiswaBeraksi {
-
+    
     public static void main(String[] args) {
         Mahasiswa mahasiswa = new Mahasiswa();
         
-        mahasiswa.membaca = "Saya sedang membaca novel";
-        mahasiswa.nyontek = "Saya pernah mencontek karena tidak paham tugas yang diberikan";
-        mahasiswa.modifikasi = "Saya melihat bendahara sedang memodifikasi laporan pengeluaran";
+        mahasiswa.membaca = "Mahasiswa itu sedang membaca materi perkuliahan";
+        mahasiswa.nyontek = "Mahasiswa terkadang menyontek karena tidak paham materinya";
+        mahasiswa.modifikasi = "Mahasiswa juga kadang memodifikasi tugasnya";
         
         System.out.println(mahasiswa.membaca);
         System.out.println(mahasiswa.nyontek);
         System.out.println(mahasiswa.modifikasi);
     }
 }
+
