32e7e0dcc8aebec332b9fd956966a81bb6276241
rivaprba <147835771+rivaprba@users.noreply.github.com>
2024-11-26 11:44:49 +0700

    Update DemoKonversiSuhu.java

--- Pertemuan 3/DemoKonversiSuhu.java
+++ Pertemuan 3/DemoKonversiSuhu.java
@@ -1,18 +1,16 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Suhu;
+package tugas2;
 
 /**
  *
- * @author khali
+ * @author ASUS
  */
 public class DemoKonversiSuhu {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         KonversiSuhu2 suhu = new KonversiSuhu2(20);
 
