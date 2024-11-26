6efd3f8af30cadb64a8f2822ffd1a59ebf530cae
rivaprba <147835771+rivaprba@users.noreply.github.com>
2024-11-26 11:19:04 +0700

    Update Matematika2.java

--- Pertemuan 3/Matematika2.java
+++ Pertemuan 3/Matematika2.java
@@ -1,1 +1,22 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Matematika2 extends Matematika {
+    int modulus;
+    
+    public void modulus (int a, int b){
+        this.a = a;
+        this.b = b;
+        modulus = this.a % this.b;
+        
+        System.out.println("Modulus\t\t\t : " + this.a + " % " + this.b + "\t= " + modulus);
+    }
+}
