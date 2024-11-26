006207389fe10749ae31e079650b929fd62e3342
rivaprba <147835771+rivaprba@users.noreply.github.com>
2024-11-26 11:44:26 +0700

    Update KonversiSuhu2.java

--- Pertemuan 3/KonversiSuhu2.java
+++ Pertemuan 3/KonversiSuhu2.java
@@ -1,12 +1,13 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Suhu;
+package tugas2;
 
 /**
  *
- * @author khali
+ * @author ASUS
  */
 public class KonversiSuhu2 extends KonversiSuhu {
     public KonversiSuhu2(double suhu) {
@@ -16,4 +17,5 @@ public class KonversiSuhu2 extends KonversiSuhu {
     public void fahrenheitToReamur() {
         System.out.println(suhu + " Fahrenheit\t\t= " + ((4 * (suhu - 32) / 9)) + " R");
     }
+    
 }
