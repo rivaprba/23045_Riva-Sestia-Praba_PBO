759d62616a77db670b6e9274b30efffb25aa4046
rivaprba <147835771+rivaprba@users.noreply.github.com>
2024-11-26 11:41:21 +0700

    Update MatematikaBeraksi.java

--- Pertemuan 2/MatematikaBeraksi.java
+++ Pertemuan 2/MatematikaBeraksi.java
@@ -1,21 +1,23 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.khalidah.matematika;
+package tugas2;
 
 /**
  *
- * @author khali
+ * @author ASUS
  */
 public class MatematikaBeraksi {
     public static void main(String[] args) {
         Matematika opMtk = new Matematika();
         
         System.out.println("\t\tOperasi Matematika");
-        opMtk.pertambahan(20, 10);
-        opMtk.pengurangan(10, 5);
-        opMtk.perkalian(10, 3);
-        opMtk.pembagian(21, 2);
+        
+        opMtk.pertambahan (20, 10);
+        opMtk.pengurangan (10, 5);
+        opMtk.perkalian (10, 3);
+        opMtk.pembagian (21, 2);
     }
 }
